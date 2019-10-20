f5fdfc00459adae4cbb816b74763dc306fc85843
nirwan <ny.unpar@gmail.com>
2019-10-20 19:05:01 +0700

    useState Hook

--- src/App.js
+++ src/App.js
@@ -1,42 +1,38 @@
-import React,{Component} from 'react';
+import React,{useState} from 'react';
 import './App.css';
 import  Person from './Person/Person';
 
-class App extends Component {
-  state = {
+const  App =  props => {
+  const [personsState, setPersonsState] = useState({
     persons : [
       {nama:'Jodi', umur:"23"},
       {nama:'Dodi', umur:"32"},
       {nama:'Kodi', umur:"25"}
     ],
     otherState : 'some other value'
-  }
+  });
 
-  GantiNamaHandler = () => {
-    //console.log('Telah diklik!');
-    //this.state.persons[0].nama= "Jodi Foster";
-    this.setState({persons:[
+  const GantiNamaHandler = () => {
+    setPersonsState({persons:[
       {nama:'Jodi Foster', umur:"23"},
       {nama:'Dodi', umur:"32"},
       {nama:'Kodi', umur:"27"},
     ]
-    })
-  }
-
-  render(){
-    return (
-      <div className="App">
-        <h1>Hai, ini aplikasi React</h1>
-        <p>Ini benar-benar Bisa</p>
-        <button
-          onClick={this.GantiNamaHandler}>Ganti Nama</button>
-        <Person nama={this.state.persons[0].nama} umur = {this.state.persons[0].umur}/>
-        <Person nama={this.state.persons[1].nama} umur = {this.state.persons[1].umur}>Hobiku: Baca</Person>
-        <Person nama={this.state.persons[2].nama} umur = {this.state.persons[2].umur}/>
-      </div>
-    );
-    //return React.createElement('div',{className:'App'},React.createElement('h1',null, 'Apakah ini bisa ditampilkan?'));
-  }
+    });
+  };
+return (
+    <div className="App">
+      <h1>Hai, ini aplikasi React</h1>
+      <p>Ini benar-benar Bisa</p>
+      <button
+        onClick={GantiNamaHandler}>Ganti Nama</button>
+      <Person nama={personsState.persons[0].nama} umur = {personsState.persons[0].umur}/>
+      <Person nama={personsState.persons[1].nama} umur = {personsState.persons[1].umur}>Hobiku: Baca</Person>
+      <Person nama={personsState.persons[2].nama} umur = {personsState.persons[2].umur}/>
+    </div>
+  );
+  //return React.createElement('div',{className:'App'},React.createElement('h1',null, 'Apakah ini bisa ditampilkan?'));
 }
+  
 
 export default App;
