f01870e9fd20c9fa5296e996c514584aa8190dc3
nirwan <ny.unpar@gmail.com>
2019-10-20 18:33:47 +0700

    gunakan state

--- src/App.js
+++ src/App.js
@@ -3,14 +3,22 @@ import './App.css';
 import  Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons : [
+      {nama:'Jodi', umur:"23"},
+      {nama:'Dodi', umur:"32"},
+      {nama:'Kodi', umur:"25"},
+    ]
+  }
   render(){
     return (
       <div className="App">
         <h1>Hai, ini aplikasi React</h1>
         <p>Ini benar-benar Bisa</p>
-        <Person nama="Jodi" umur = "23"/>
-        <Person nama="Dodi" umur = "32">Hobiku: Baca</Person>
-        <Person nama="Kodi" umur = "23"/>
+        <button>Ganti Nama</button>
+        <Person nama={this.state.persons[0].nama} umur = {this.state.persons[0].umur}/>
+        <Person nama={this.state.persons[1].nama} umur = {this.state.persons[1].umur}>Hobiku: Baca</Person>
+        <Person nama={this.state.persons[2].nama} umur = {this.state.persons[2].umur}/>
       </div>
     );
     //return React.createElement('div',{className:'App'},React.createElement('h1',null, 'Apakah ini bisa ditampilkan?'));
