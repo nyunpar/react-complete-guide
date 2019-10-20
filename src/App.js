4eaaa4fabeba2ad4beaaa22ada21da83f38af984
nirwan <ny.unpar@gmail.com>
2019-10-20 19:39:16 +0700

    gunakan bind()

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ class App extends Component {
     otherState : 'some other value'
   }
 
-  GantiNamaHandler = () => {
+  GantiNamaHandler = (NamaBaru) => {
     //console.log('Telah diklik!');
     //this.state.persons[0].nama= "Jodi Foster";
     this.setState({persons:[
-      {nama:'Jodi Foster', umur:"23"},
+      {nama: NamaBaru, umur:"23"},
       {nama:'Dodi', umur:"32"},
       {nama:'Kodi', umur:"27"},
     ]
@@ -29,10 +29,17 @@ class App extends Component {
         <h1>Hai, ini aplikasi React</h1>
         <p>Ini benar-benar Bisa</p>
         <button
-          onClick={this.GantiNamaHandler}>Ganti Nama</button>
-        <Person nama={this.state.persons[0].nama} umur = {this.state.persons[0].umur}/>
-        <Person nama={this.state.persons[1].nama} umur = {this.state.persons[1].umur}>Hobiku: Baca</Person>
-        <Person nama={this.state.persons[2].nama} umur = {this.state.persons[2].umur}/>
+          onClick={this.GantiNamaHandler.bind(this,'Fodi')}>Ganti Nama</button>
+        <Person 
+          nama={this.state.persons[0].nama} 
+          umur = {this.state.persons[0].umur}/>
+        <Person 
+          nama={this.state.persons[1].nama} 
+          umur = {this.state.persons[1].umur}
+          click ={this.GantiNamaHandler.bind(this,'Rodi')}>Hobiku: Baca</Person>
+        <Person 
+          nama={this.state.persons[2].nama} 
+          umur = {this.state.persons[2].umur}/>
       </div>
     );
     //return React.createElement('div',{className:'App'},React.createElement('h1',null, 'Apakah ini bisa ditampilkan?'));
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,7 +4,7 @@ import React from 'react';
 const person = (props)=> {
     return (
         <div>
-            <p>Saya {props.nama} dan saya berumur {props.umur} tahun</p>
+            <p onClick ={props.click}> Saya {props.nama} dan saya berumur {props.umur} tahun</p>
             <p>{props.children}</p>
         </div>
 
