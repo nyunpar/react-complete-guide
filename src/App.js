d0c37a4bd7541e361dce0825faaebac2aabb25ab
nirwan <ny.unpar@gmail.com>
2019-10-20 18:06:25 +0700

    buat komponen person

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React,{Component} from 'react';
 import './App.css';
+import  Person from './Person/Person';
 
 class App extends Component {
   render(){
     return (
       <div className="App">
         <h1>Hai, ini aplikasi React</h1>
+        <p>Ini benar-benar Bisa</p>
+        <Person/>
       </div>
     );
     //return React.createElement('div',{className:'App'},React.createElement('h1',null, 'Apakah ini bisa ditampilkan?'));
---
+++ src/Person/Person.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const person = ()=> {
+    return <p>Saya person!</p>
+}
+
+export default person;
