62f6f32d7befa2e3f01ae8445f9f17537a61d6d7
nirwan <ny.unpar@gmail.com>
2019-10-27 10:04:31 +0700

    radium pseudo

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React,{Component} from 'react';
+import Radium from 'radium';
 import './App.css';
 import  Person from './Person/Person';
 
@@ -39,7 +40,11 @@ class App extends Component {
       font:'inherit',
       border: '1px solid blue',
       padding :'8px',
-      cursor:'pointer'
+      cursor:'pointer',
+      ':hover':{
+        backgroundColor:'lightgreen',
+        color:'black'
+      }
     };
     let persons = null;
     if (this.state.showPersons){
@@ -56,6 +61,10 @@ class App extends Component {
         </div> 
       );
       style.backgroundColor = 'red';
+      style[':hover'] = {
+        backgroundColor:'salmon',
+        color:'black'
+      }
     }
 
     let classes =[];
@@ -81,4 +90,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default Radium(App);
